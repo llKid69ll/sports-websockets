04b1bb3bb57368cc264154f8b72fce85485ad63a
Lalit kumar <kidlalit786@gmail.com>
2026-02-15 01:36:18 +0530

    implement pub sub broadcasting for commentaries

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {matchRouter} from "./routes/matches.js";
 import http from 'http';
 import {attachWebSocketServer} from "./ws/server.js";
 import {securityMiddleware} from "./arcjet.js";
+import {commentaryRouter} from "./routes/commentary.js";
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || '0.0.0.0';
@@ -20,10 +21,12 @@ app.get('/', (req, res) => {
 
 app.use(securityMiddleware());
 
-app.use('/matches', matchRouter)
+app.use('/matches', matchRouter);
+app.use('/matches/:id/commentary', commentaryRouter);
 
-const { broadcastMatchCreated } = attachWebSocketServer(server);
+const { broadcastMatchCreated , broadcastCommentary} = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary= broadcastCommentary;
 
 // Start server
 server.listen(PORT, HOST,() => {
